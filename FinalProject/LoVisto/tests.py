f4946312f690fd08978b01b97f3a33f8f8196866
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2021-07-08 17:58:04 +0200

    req + tests

--- FinalProject/LoVisto/tests.py
+++ FinalProject/LoVisto/tests.py
@@ -3,9 +3,6 @@ from django.test import TestCase, Client
 # Create your tests here.
 
 from django.test import TestCase
-from . import aemet_parser
-from . import redditParser
-from . import YTparser
 from django.test import Client
 
 class GetTests(TestCase):
@@ -44,6 +41,36 @@ class GetTests(TestCase):
         content = response.content.decode('utf-8')
         self.assertIn('<p align="center">THIS WEBSITE IS ABLE ONLY FOR AUTHENTICATED USERS</p>', content)
 
+class PostTests(TestCase):
 
+    def test_login(self):
+        user_name = 'usuario'
+        password = 'user'
+        login = 'login'
+        c = Client()
+        response = c.post('/LoVisto/', {'action': login,'username': user_name, 'password': password})
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/LoVisto/')
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        self.assertIn('<h2>Menú</h2>', content)
 
+    def test_logout(self):
+        logout = 'logout'
+        c = Client()
+        response = c.post('/LoVisto/', {'action': logout})
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/LoVisto/')
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        self.assertIn('<h2>Menú</h2>', content)
 
+    def test_changeMode(self):
+        action = 'changeMode'
+        c = Client()
+        response = c.post('/LoVisto/', {'action': action})
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/LoVisto/')
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        self.assertIn('<h2>Menú</h2>', content)
